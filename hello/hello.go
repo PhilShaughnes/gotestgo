3643b8fc7e1f922bc453e6066682dde626d38a83
Phil Shaughnessy <philip.shaughnessy@gmail.com>
2024-02-24 12:19:41 -0500

    Update to be multilingual

--- hello/hello.go
+++ hello/hello.go
@@ -4,14 +4,34 @@ import (
 	"fmt"
 )
 
+type Lang int
+
 const (
 	englishPrefix = "Hello, "
+	spanishPrefix = "Hola, "
+	frenchPrefix  = "Bonjour, "
+	english       = Lang(iota)
+	spanish
+	french
 )
 
-func Hello(name string) string {
-	return englishPrefix + name
+func Hello(name string, l Lang) string {
+	var prefix string
+	switch l {
+	case spanish:
+		prefix = spanishPrefix
+	case french:
+		prefix = frenchPrefix
+	default:
+		prefix = englishPrefix
+	}
+
+	if name == "" {
+		name = "World"
+	}
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", english))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,35 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Phil")
-	want := "Hello, Phil"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("Say hello to provided name", func(t *testing.T) {
+		got := Hello("Phil", english)
+		want := "Hello, Phil"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, World' when no name is provided", func(t *testing.T) {
+		got := Hello("", english)
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("use french", func(t *testing.T) {
+		got := Hello("", french)
+		want := "Bonjour, World"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("use spanish", func(t *testing.T) {
+		got := Hello("", spanish)
+		want := "Hola, World"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
